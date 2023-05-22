46d25e859cd7b5e2a37efa5225c2608c7d4d2f4a
Hafizur Rahman <hafiz.ubikm@gmail.com>
2023-05-22 23:44:45 +0600

    added questions answers at readme

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/userinfo', [UserController::class, 'userInfo']);
 Route::post('/fileupload', [UserController::class, 'fileUpload']);
 
+/*
+Question 7:
+Create a route in Laravel that handles a POST request to the '/submit' URL. Inside the route closure, 
+retrieve the 'email' input parameter from the request and store it in a variable called $email. 
+Return a JSON response with the following data:
+{
+    "success": true,
+    "message": "Form submitted successfully."
+}
+*/
 Route::post('/submit', function (Request $request) {
     $email = $request->input('email');
 
